1b3ce2bc29f17c49e77edc19cffaf7f1b974eac3
Geroen Joris <geroen@gmail.com>
2026-02-28 14:23:35 +0100

    refactor: extract types to separate module

---
+++ src/renderer/types/index.ts
@@ -0,0 +1,18 @@
+export type DownloadType = 'mp3' | 'video';
+
+export interface BinaryProgress {
+  binary: string;
+  progress: number;
+  status: string;
+}
+
+export interface Settings {
+  downloadPath: string;
+}
+
+export interface DownloadOptions {
+  format: string;
+  extractAudio?: boolean;
+  audioFormat?: string;
+  output: string;
+}
